2fdf1faefd77dcfd0c9c8f99f36eac6640824701
Henryck <henryck.leandro@outlook.com>
2023-06-14 19:42:24 -0300

    add eslint

--- NodeApi/src/server.ts
+++ NodeApi/src/server.ts
@@ -2,12 +2,12 @@ interface User {
   Name: string
 }
 
-function SayHello (user: User) {
-  console.log(`Hello, ${user.Name}`);
+function SayHello (user: User): void {
+  console.log(`Hello, ${user.Name}`)
 }
 
 const user: User = {
-  Name: "Henryck"
+  Name: 'Henryck'
 }
 
-SayHello(user);
+SayHello(user)
