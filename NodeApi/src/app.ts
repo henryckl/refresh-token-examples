6c85d8055168ead330553f921f5c41b8acfef4e3
Henryck <henryck.leandro@outlook.com>
2023-06-15 20:38:22 -0300

    add environment variables

--- NodeApi/src/app.ts
+++ NodeApi/src/app.ts
@@ -1,12 +1,12 @@
 import fastify from 'fastify'
-
+import { env } from './env'
 const app = fastify()
 
 app.get('/user', async (request, reply) => {
   return 'user name\n'
 })
 
-app.listen({ port: 3333 }, (err, address) => {
+app.listen({ port: env.PORT }, (err, address) => {
   if (err != null) {
     console.error(err)
     process.exit(1)
